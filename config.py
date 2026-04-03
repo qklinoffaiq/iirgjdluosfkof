025374c36d4e6a8cbd55dcfc094a1c05cb8d8e79
QKlinoffAIQ <mersikabo@gmail.com>
2026-04-03 20:43:17 +0300

    config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 group_token = 'vk1.a.JI_V6c9DeMjZKuQgJ_Cds9Cm4nQsigtgokp7cq3fFyAJi9unE-I1u_yM0uu3_1J_2NuY3uEO_UfnLj4Uf_-28Tt70OAMZmEiMz-TaAlQkvhX-FJV1HO-QqB6Bxtxb8s0Fc6iNN1z0YllC2THpiq_Ue-gPUc4RKxaO5iHE_MrbmQT591luFv6FdbobquV_C0hRh9D43f5r6CTTPNyqUdl7A'
 group_id = 237303045
 
-admin_ids = [574393629, 597064735]  # ID администраторов (обновлено)
+admin_ids = [574393629, 597064735, 488828183]  # ID администраторов (обновлено)
 
 cd_min = 10
 interval_sec = 0.1
